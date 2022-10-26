7a57d10733569afd4d653f17c7d4f7e453e11ada
slyguy5646 <lpmonaghan06@yahoo.com>
2022-10-26 15:19:10 -0400

    changes

--- cudaTest.py
+++ cudaTest.py
@@ -1,12 +1,15 @@
 from pupil_apriltags import Detector, Detection
 import cv2
 import numpy as np
-from numba import jit, cuda
+from numba import jit
 
-@jit(target_backend="cuda")
-def main():
+
+
+    # hello = cv2.namedWindow("Image")
+
+def main():    
     at = Detector(
-        nthreads=20
+        nthreads=100
     )
 
     cameraMatrix = (1386.5059636831022, 1380.3287074149298, 674.3138223396147, 431.0716867204328)
@@ -18,12 +21,20 @@ def main():
         success, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
+        #cv2.imshow('frame', frame)
         if not success:
             break
 
         tagDetection = at.detect(img=gray, estimate_tag_pose=True, camera_params=cameraMatrix, tag_size=0.060325)
 
         print(tagDetection)
+            # cv2.imshow(hello, gray)
+            # k = cv2.waitKey(1)
 
-if __name__=="__main__":
+            # if k == 27:
+            #     break
+        
+
+if __name__ == "__main__":
     main()
+
